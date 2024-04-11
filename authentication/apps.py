1645e831bf51054b5e80e30b07b21fd3565774f4
Ali Bowens <abowens1@umbc.edu>
2024-04-11 14:48:32 -0400

    This is the commit theat I made

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JudgesLoginConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'judges_login'
+    name = 'authentication'
--- cbic/urls.py
+++ cbic/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_app project.
+URL configuration for cbic project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('judges_login.urls'))
+    path('', include('authentication.urls')),
 ]

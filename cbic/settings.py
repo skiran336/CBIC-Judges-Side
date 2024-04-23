44d8cfbed7277c7acebabe9b8a0ea090d59428a7
skiran336 <163210382+skiran336@users.noreply.github.com>
2024-04-22 21:40:10 -0400

    settings update

--- cbic/settings.py
+++ cbic/settings.py
@@ -130,4 +130,6 @@ STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

e42a4fde4e7b1e15cb940188b56f7058311c8113
skiran336 <163210382+skiran336@users.noreply.github.com>
2024-05-09 20:51:24 -0400

    home update

--- authentication/views.py
+++ authentication/views.py
@@ -85,7 +85,7 @@ def signout(request):
 def save_data(request):
     if request.method == "POST":
         # Get the form data
-        student_id = request.POST.get("_id")
+        student_id = request.POST.get("student_id")
         identify = request.POST.get("identify")
         impact = request.POST.get("Impact")
         competitive = request.POST.get("Competitive")

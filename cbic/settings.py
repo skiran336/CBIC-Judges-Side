45b5039dd276d0dd748a9cd30659d4cb2562b9dc
skiran336 <163210382+skiran336@users.noreply.github.com>
2024-05-11 07:01:07 -0400

    email settings update

--- cbic/settings.py
+++ cbic/settings.py
@@ -88,6 +88,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'saikk98@gmail.com'
+EMAIL_HOST_PASSWORD = 'bhagavathi40'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

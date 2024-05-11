cc311f4efd816f1554d87757d1c1c9f97bd2d99d
skiran336 <163210382+skiran336@users.noreply.github.com>
2024-05-11 06:57:05 -0400

    reset password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.signin, name="signin"),
@@ -8,5 +9,10 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signout', views.signout, name="signout"),
     path('forgot_password', views.signup, name="forgot_password"),
-    path('scorepage', views.scorepage, name="scorepage")
+    path('scorepage', views.scorepage, name="scorepage"),
+    path('forgot_password/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset_form.html'), name='forgot_password'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
